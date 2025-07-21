cdce5d092b7b32901a9c47f92536827812c5f039
ahmedsm504 <ahmedsm504@gmail.com>
2025-07-21 09:35:09 -0700

    Fix login redirect issue and add custom login page

--- designs/urls.py
+++ designs/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 from django.contrib.auth.views import LogoutView
-
+from .views import custom_login_view
 from .views import profile_view, delete_account
 
 urlpatterns = [
@@ -12,7 +12,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('design/<int:pk>/', views.design_detail, name='design_detail'),
     path('designer/<str:username>/', views.designer_profile, name='designer_profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='designs/login.html'), name='login'),
+    path('login/', custom_login_view, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('designs/<int:pk>/edit/', views.edit_design, name='edit_design'),
     path('password/', auth_views.PasswordChangeView.as_view(
